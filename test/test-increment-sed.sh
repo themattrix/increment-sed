a6b33656936d2b46b248006a2cf4f742563e6c67
Matthew Tardiff <mattrix@gmail.com>
2014-03-25 21:16:44 -0500

    Fixed a bug involving adding numbers >100 on the right-hand-side and improved the unit tests.

--- test/test-increment-sed.sh
+++ test/test-increment-sed.sh
@@ -1,20 +1,48 @@
 #!/usr/bin/env bash
 
-readonly THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+#
+# Tests
+#
 
-function increment_sed() {
-    "${THIS_DIR}/../src/increment.sed"
+function test_carry_once() {
+    diff_output <(
+        increment_sed <<< "9+."
+    ) <(
+        echo "10"
+    )
 }
 
-function test_increment_from_0_to_10000() {
-    diff <(
-        for (( i = 0; i < 10000; i++ )); do
-            echo "${i}+."
-            echo "${i}+.."
-            echo "${i}+..."
+function test_carry_twice() {
+    diff_output <(
+        increment_sed <<< "99+."
+    ) <(
+        echo "100"
+    )
+}
+
+function test_carry_three_times() {
+    diff_output <(
+        increment_sed <<< "999+."
+    ) <(
+        echo "1000"
+    )
+}
+
+function test_add_from_0_to_111() {
+    local limit=111
+
+    diff_output <(
+        for ((i = 0; i <= "${limit}"; i++)); do
+            {
+                printf "0+%${i}s\n" ''
+                printf "1+%${i}s\n" ''
+                printf "2+%${i}s\n" ''
+                printf "3+%${i}s\n" ''
+            } | sed 's/ /./g'
         done | increment_sed
     ) <(
-        for (( i = 0; i < 10000; i++ )); do
+        for ((i = 0; i <= "${limit}"; i++)); do
+            echo "${i}"
             echo "$((i + 1))"
             echo "$((i + 2))"
             echo "$((i + 3))"
@@ -22,4 +50,84 @@ function test_increment_from_0_to_10000() {
     )
 }
 
-test_increment_from_0_to_10000
+function test_increment_from_0_to_111() {
+    local limit=111
+
+    diff_output <(
+        for ((i = 0; i <= "${limit}"; i++)); do
+            echo "${i}+"
+            echo "${i}+."
+            echo "${i}+.."
+            echo "${i}+..."
+        done | increment_sed
+    ) <(
+        for ((i = 0; i <= "${limit}"; i++)); do
+            echo "${i}"
+            echo "$((i + 1))"
+            echo "$((i + 2))"
+            echo "$((i + 3))"
+        done
+    )
+}
+
+
+#
+# Test helpers
+#
+
+readonly THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+function increment_sed() {
+    "${THIS_DIR}/../src/increment.sed"
+}
+
+function diff_output() {
+    diff -y --width=12 --suppress-common-lines "$@"
+}
+
+function run() {
+    test_fn="$1"
+
+    output=$("${test_fn}" 2>&1)
+    status=$?
+
+    ((test_count++))
+
+    if [ ${status} -eq 0 ]
+    then
+        echo "[pass] ${test_fn}"
+    else
+        ((fail_count++))
+
+        echo "[FAIL] ${test_fn}:"
+        sed 's/^/    /' <<< "${output}"
+        echo
+    fi
+}
+
+function test_summary() {
+    echo
+    if [ ${fail_count} -eq 0 ]
+    then
+        echo ">>> success (${test_count} tests)"
+    else
+        echo ">>> FAILURE (${fail_count}/${test_count} tests failed)"
+        exit 1
+    fi
+}
+
+
+#
+# Test runner
+#
+
+test_count=0
+fail_count=0
+
+run test_carry_once
+run test_carry_twice
+run test_carry_three_times
+run test_add_from_0_to_111
+run test_increment_from_0_to_111
+
+test_summary
